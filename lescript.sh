7dbbbfa3f55212bb4df0855d3a16368c245fb26c
Nimeryon <t.auriol46@gmail.com>
2020-02-28 13:08:49 +0100

    version3.5 pas de test d'erreur autre le lock et implémentaiton du log

--- lescript.sh
+++ lescript.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+
+
 # Est ce que in pas dans la liste de /tmp/
 # Alors créer in/
-if ! ls /tmp/ | grep in
+if ! ls /tmp/ | grep in 1> /dev/null
   then
   cd /tmp/ ; mkdir in
 fi
 
 # Est ce que out pas dans la liste de /tmp/
 # Alors créer out/
-if ! ls /tmp/ | grep out
+if ! ls /tmp/ | grep out 1> /dev/null
   then
   cd /tmp/ ; mkdir out
 fi
 
-ls -a /tmp/in/ | gzip > /tmp/out/tmpIn.gz
+if ! ls /tmp/out/ | grep lock /dev/null
+  then
+  # Création du fichier lock temporaire
+  cd /tmp/out/ ; echo "locked" > lock
 
-exit 0
+  # Ajout dans le log
+  date >> /tmp/out/log
+  ls /tmp/in/* >> /tmp/out/log
+
+  ls /tmp/in/* | gzip > /tmp/out/tmpIn.gz
+  echo "Les dossier de /tmp/in/ sont listé dans /tmp/out/tmpIn.gz"
+  
+  # Suppréssion du fichier lock temporaire
+  cd /tmp/out/ ; rm lock
+  exit 0
+else
+  echo "Le fichier /tmp/out/ est lock"
+  exit 22
+fi
