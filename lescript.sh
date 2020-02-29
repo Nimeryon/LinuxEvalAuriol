fd3846158cfe50fdfb68416c594e4f83218a8e7f
Nimeryon <t.auriol46@gmail.com>
2020-03-01 00:04:17 +0100

    Version 4

--- lescript.sh
+++ lescript.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # Est ce que in pas dans la liste de /tmp/
 # Alors créer in/
 if ! ls /tmp/ | grep in 1> /dev/null
@@ -16,17 +14,31 @@ if ! ls /tmp/ | grep out 1> /dev/null
   cd /tmp/ ; mkdir out
 fi
 
-if ! ls /tmp/out/ | grep lock /dev/null
+if ! ls /tmp/out/ | grep lock 1> /dev/null
   then
   # Création du fichier lock temporaire
   cd /tmp/out/ ; echo "locked" > lock
 
+  if ! [ "$(ls -A /tmp/in/)" ]
+    then
+    date >> /tmp/out/log
+    echo "Erreur : aucun fichier n'as était trouvé dans /tmp/in/" >> /tmp/out/log  
+    echo "Erreur : aucun fichier n'as était trouvé dans /tmp/in/"
+    exit 2
+  fi
+
   # Ajout dans le log
   date >> /tmp/out/log
-  ls /tmp/in/* >> /tmp/out/log
+  ls -A /tmp/in/* >> /tmp/out/log
 
-  ls /tmp/in/* | gzip > /tmp/out/tmpIn.gz
-  echo "Les dossier de /tmp/in/ sont listé dans /tmp/out/tmpIn.gz"
+  if ! (gzip -r /tmp/in/* ; mv /tmp/in/* /tmp/out/ 2> /dev/null)
+    then
+    echo "Un problême est survenue lors de la compréssion ou du transfert" >> /tmp/out/log
+    echo "Un problème est survenue lors de la compréssion ou du transfert"
+    exit 2
+  fi
+
+  echo "Les dossier de /tmp/in/ ont étais compréssé puis envoyé dans /tmp/out/tmpIn.gz"
   
   # Suppréssion du fichier lock temporaire
   cd /tmp/out/ ; rm lock
